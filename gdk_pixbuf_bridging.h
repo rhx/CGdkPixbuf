1ad4ea2c7865f18e07209a144f487af56cbe8ca4
Rene Hexel <r.hexel@griffith.edu.au>
2016-05-29 15:46:33 +1000

    Add missing includes

--- gdk_pixbuf_bridging.h
+++ gdk_pixbuf_bridging.h
@@ -1,2 +1,6 @@
 #define GLIB_DISABLE_DEPRECATION_WARNINGS
 #include <gdk-pixbuf/gdk-pixbuf.h>
+#include <gdk-pixbuf/gdk-pixbuf-loader.h>
+#include <gdk-pixbuf/gdk-pixbuf-marshal.h>
+#include <gdk-pixbuf/gdk-pixbuf-transform.h>
+#include <gdk-pixbuf/gdk-pixdata.h>
