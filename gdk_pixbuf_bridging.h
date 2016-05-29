739a1ab612f593b5930e3e4e5b1e5b64bbf14aa2
Rene Hexel <r.hexel@griffith.edu.au>
2016-05-30 06:22:30 +1000

    Add empty, private structs so they get properly exported to Swift

--- gdk_pixbuf_bridging.h
+++ gdk_pixbuf_bridging.h
@@ -1,3 +1,10 @@
+struct _GdkPixbuf {};
+struct _GdkPixbufAnimation {};
+struct _GdkPixbufAnimationIter {};
+struct _GdkPixbufFormat {};
+struct _GdkPixbufSimpleAnim {};
+struct _GdkPixbufSimpleAnimClass {};
+
 #define GLIB_DISABLE_DEPRECATION_WARNINGS
 #include <gdk-pixbuf/gdk-pixbuf.h>
 #include <gdk-pixbuf/gdk-pixbuf-loader.h>
