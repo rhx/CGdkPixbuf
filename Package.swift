a71581c10e3c1d77cb2bfbe1c7f529d801e28b14
Rene Hexel <r.hexel@griffith.edu.au>
2018-12-14 08:50:56 +1000

    Update to Swift 4 package format

--- Package.swift
+++ Package.swift
@@ -1,3 +1,11 @@
+// swift-tools-version:4.0
+
 import PackageDescription 
 
-let package = Package(name: "CGdkPixbuf")
+let package = Package(name: "CGdkPixbuf",
+	pkgConfig: "gdk-pixbuf-2.0 gio-unix-2.0 glib-2.0",
+	providers: [
+		.brew(["gdk-pixbuf", "glib", "glib-networking", "gobject-introspection"]),
+		.apt(["libgdk-pixbuf2.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
+	]
+)
